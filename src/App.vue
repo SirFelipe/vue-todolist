4b66951fe77bf98317c52fb223e9ed18a5cd26b2
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-09 23:02:01 -0300

    Creating a to do list, working with v-for, splice method

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    TO DO LIST:
+    <Entrada v-on:mudou="digitou"/>
+    <br>
+    <Button texto="Inserir" classe="btn btn-primary" fundo="" v-on:clicou="this.inserir"/>
+    <br>
+    <div :key="indice" v-for="(valor, indice) in lista">
+      <a style="{color: #b3e6ff}" href="#" v-on:click="remover(indice)">{{valor}}</a>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Entrada from './components/Entrada.vue'
+import Button from './components/Button.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Entrada,
+    Button
+  },
+  data(){
+    return { 
+      valor: "",
+      lista: []
+    }
+  },
+  methods:{
+    digitou(txt){
+      this.valor = txt;
+    },
+    remover(idx){
+                //MÉTODO MAIS BIXÃO QUE EXISTE
+                //O BIXO REMOVE E ADICIONA
+                //     Indice, Quantidade a ser removida, ["Adicionar1","Adicionar2"]
+      this.lista.splice(idx, 1);
+    },
+    inserir(){
+      this.lista.push(this.valor);
+    }
   }
 }
 </script>
+<style lang="scss">
+      @import '../node_modules/bootstrap/scss/bootstrap.scss';
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+      #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+      }
 </style>
---
+++ src/components/Entrada.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="col-md-12">
+            <p></p>
+            <input type="text" v-model="txt" v-on:change="enter"/>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    data(){
+       return { txt: "" }
+    },methods:{
+        enter(){
+            this.$emit("mudou", this.txt);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
